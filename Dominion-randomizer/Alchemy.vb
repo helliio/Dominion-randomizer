71f471c449316179865babb3ee8ff1bd70aff7f8
helliio <liang150393@gmail.com>
2016-10-21 13:51:27 +0200

    Removed React type

--- Dominion-randomizer/Alchemy.vb
+++ Dominion-randomizer/Alchemy.vb
@@ -1,17 +1,17 @@
-﻿'Format: <sort prio>.<name>.<cost>.<is attack>.<is curser>.<is react>
+﻿'Format: <sort prio>.<name>.<cost>.<is attack>.<is curser>
 Public Class Alchemy
     Public Shared cards As New SortedSet(Of String) From {
-        "00.Transmute.P.0.0.0",
-        "00.Vineyard.P.0.0.0",
-        "02.Herbalist.2.0.0.0",
-        "02.Apothecary.2P.0.0.0",
-        "02.Scrying Pool.2P.1.0.0",
-        "02.University.2P.0.0.0",
-        "03.Alchemist.3P.0.0.0",
-        "03.Familiar.3P.1.1.0",
-        "03.Philosopher's Stone.3P.0.0.0",
-        "04.Golem.4P.0.0.0",
-        "05.Apprentice.5.0.0.0",
-        "06.Possession.6P.0.0.0"
+        "00.Transmute.P.0.0",
+        "00.Vineyard.P.0.0",
+        "02.Herbalist.2.0.0",
+        "02.Apothecary.2P.0.0",
+        "02.Scrying Pool.2P.1.0",
+        "02.University.2P.0.0",
+        "03.Alchemist.3P.0.0",
+        "03.Familiar.3P.1.1",
+        "03.Philosopher's Stone.3P.0.0",
+        "04.Golem.4P.0.0",
+        "05.Apprentice.5.0.0",
+        "06.Possession.6P.0.0"
     }
 End Class
--- Dominion-randomizer/BaseSet.vb
+++ Dominion-randomizer/BaseSet.vb
@@ -1,41 +1,41 @@
-﻿'Format: <sort prio>.<name>.<cost>.<is attack>.<is curser>.<is react>
+﻿'Format: <sort prio>.<name>.<cost>.<is attack>.<is curser>
 Public Class BaseSet
     Public Shared cards_common As New SortedSet(Of String) From {
-        "02.Cellar.2.0.0.0",
-        "02.Chapel.2.0.0.0",
-        "02.Moat.2.0.0.1",
-        "03.Village.3.0.0.0",
-        "03.Workshop.3.0.0.0",
-        "04.Bureaucrat.4.1.0.0",
-        "04.Gardens.4.0.0.0",
-        "04.Militia.4.1.0.0",
-        "04.Moneylender.4.0.0.0",
-        "04.Remodel.4.0.0.0",
-        "04.Smithy.4.0.0.0",
-        "04.Throne Room.4.0.0.0",
-        "05.Council Room.5.0.0.0",
-        "05.Festival.5.0.0.0",
-        "05.Laboratory.5.0.0.0",
-        "05.Library.5.0.0.0",
-        "05.Market.5.0.0.0",
-        "05.Mine.5.0.0.0",
-        "05.Witch.5.1.1.0"
+        "02.Cellar.2.0.0",
+        "02.Chapel.2.0.0",
+        "02.Moat.2.0.0",
+        "03.Village.3.0.0",
+        "03.Workshop.3.0.0",
+        "04.Bureaucrat.4.1.0",
+        "04.Gardens.4.0.0",
+        "04.Militia.4.1.0",
+        "04.Moneylender.4.0.0",
+        "04.Remodel.4.0.0",
+        "04.Smithy.4.0.0",
+        "04.Throne Room.4.0.0",
+        "05.Council Room.5.0.0",
+        "05.Festival.5.0.0",
+        "05.Laboratory.5.0.0",
+        "05.Library.5.0.0",
+        "05.Market.5.0.0",
+        "05.Mine.5.0.0",
+        "05.Witch.5.1.1"
     }
     Public Shared cards_1ed As New SortedSet(Of String) From {
-        "03.Chancellor.3.0.0.0",
-        "03.Woodcutter.3.0.0.0",
-        "04.Feast.4.0.0.0",
-        "04.Thief.4.1.0.0",
-        "04.Spy.4.1.0.0",
-        "06.Adventurer.6.0.0.0"
+        "03.Chancellor.3.0.0",
+        "03.Woodcutter.3.0.0",
+        "04.Feast.4.0.0",
+        "04.Thief.4.1.0",
+        "04.Spy.4.1.0",
+        "06.Adventurer.6.0.0"
     }
     Public Shared cards_2ed As New SortedSet(Of String) From {
-        "03.Harbinger.3.0.0.0",
-        "03.Merchant.3.0.0.0",
-        "03.Vassal.3.0.0.0",
-        "04.Poacher.4.0.0.0",
-        "05.Bandit.5.1.0.0",
-        "05.Sentry.5.0.0.0",
-        "06.Artisan.6.0.0.0"
+        "03.Harbinger.3.0.0",
+        "03.Merchant.3.0.0",
+        "03.Vassal.3.0.0",
+        "04.Poacher.4.0.0",
+        "05.Bandit.5.1.0",
+        "05.Sentry.5.0.0",
+        "06.Artisan.6.0.0"
     }
 End Class
